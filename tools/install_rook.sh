af776215a33ff9142980411a7fc088672944aa4a
Λlisue <lambdalisue@hashnote.net>
2015-05-01 04:01:45 +0900

    Update install_rook.sh

--- tools/install_rook.sh
+++ tools/install_rook.sh
@@ -5,7 +5,7 @@ if [[ ! -d "$HOME/.homesick/repos/homeshick" ]]; then
     printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.sh"' >> $HOME/.bashrc
 fi
 source $HOME/.homesick/repos/homeshick/homeshick.sh
-homeshick clone git@github.com:lambdalisue/rook
+homeshick clone git@github.com:lambdalisue/rook -f
 homeshick cd rook
 ./tools/install_requirements.sh
 
