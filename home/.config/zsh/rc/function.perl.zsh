a9de7419dc0ffca99bc625c2a1ca5e8f168faa14
lambdalisue <lambdalisue@hashnote.net>
2015-06-15 15:40:27 +0900

    Fix perl_find_module_version

--- home/.config/zsh/rc/function.perl.zsh
+++ home/.config/zsh/rc/function.perl.zsh
@@ -1,6 +1,6 @@
 function perl_find_module_version() {
     local name="$1"
-    perl -M$name -e "print \$$name::VERSION . '\n'"
+    perl -M$name -e "print \$$name::VERSION . \"\n\""
 }
 function perl_find_installed_modules() {
     local collect_modules="${ZDOTDIR}/tools/perl/collect-modules.pl"
