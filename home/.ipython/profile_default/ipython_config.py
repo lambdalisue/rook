738e5607d9e14a8d0220f9835e3b5f59a131f465
lambdalisue <lambdalisue@hashnote.net>
2015-06-26 14:58:53 +0900

    Add config of IPython

---
+++ home/.ipython/profile_default/ipython_config.py
@@ -0,0 +1,10 @@
+c = get_config()
+
+c.InteractiveShellApp.exec_lines = [
+    'import numpy as np',
+    'import scipy as sp',
+    'import pandas as pd',
+    'import matplotlib.pyplot as pl',
+]
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.editor = 'vim'
