1b423802923cf68cb4c9b5bc4d3bb413ac733535
lambdalisue <lambdalisue@hashnote.net>
2015-08-26 13:26:49 +0900

    Update install script

--- tools/install_rook.sh
+++ tools/install_rook.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if ! type git > /dev/null 2>&1; then
+    echo "'git' is required. Install it first."
+    exit 1
+fi
+
 if [[ ! -d "$HOME/.homesick/repos/homeshick" ]]; then
     git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
     printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.sh"' >> $HOME/.bashrc
@@ -12,7 +17,7 @@ if [[ ! -d "$HOME/.homesick/repos/rook" ]]; then
         homeshick clone git@github.com:lambdalisue/rook -f --batch
     fi
 fi
-homeshick cd rook
+cd $HOME/.homesick.repos/rook
 bash ./tools/install_requirements.sh
 
 if [[ "$(uname)" = "Linux" ]]; then
