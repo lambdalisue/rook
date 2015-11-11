960992a6fb0e3f8509a5e93497773c565225d80b
lambdalisue <lambdalisue@hashnote.net>
2015-11-12 00:37:46 +0900

    Add 'static' for serving static http

---
+++ home/.config/zsh/rc/function.static.zsh
@@ -0,0 +1,21 @@
+function static() {
+    local PORT=$1
+    if [[ -z $PORT ]]; then
+        PORT=8000
+    fi
+    if type python3 > /dev/null 2>&1; then
+        python3 -m http.server $PORT
+    elif type python2 > /dev/null 2>&1; then
+        python2 -m SimpleHTTPServer $PORT
+    elif type python > /dev/null 2>&1; then
+        local VERSION=$(python -c "import sys; print(sys.version_info.major)")
+        if [[ "$VERSION" == "3" ]]; then
+            python3 -m http.server $PORT
+        else
+            python2 -m SimpleHTTPServer $PORT
+        fi
+    else
+        echo "'static' require python 2.x or 3.x but both is not installed."
+        exit 1
+    fi
+}
