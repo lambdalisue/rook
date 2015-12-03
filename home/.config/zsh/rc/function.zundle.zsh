3b37582a79cad5b04f6beda186d78b71e1e954e2
lambdalisue <lambdalisue@hashnote.net>
2015-12-03 15:42:52 +0900

    Add zundle_update function

--- home/.config/zsh/rc/function.zundle.zsh
+++ home/.config/zsh/rc/function.zundle.zsh
@@ -4,19 +4,19 @@
 # Author:   Alisue <lambdalisue@hashnote.net>
 # License:  MIT License
 #
-zundle_get_bundle_path() {
+function zundle_get_bundle_path() {
     local name=$1
     local bundle_root
     zstyle -s ":zundle:root" bundle bundle_root
     echo -en "${bundle_root}/${name}"
 }
-zundle_get_config_path() {
+function zundle_get_config_path() {
     local name=$1
     local config_root
     zstyle -s ":zundle:root" config config_root
     echo -en "${config_root}/${name}.zsh"
 }
-zundle_install() {
+function zundle_install() {
     local url=$1
     local name=$2
     # complement github url
@@ -34,7 +34,14 @@ zundle_install() {
     fi
     zundle_configure $name
 }
-zundle_configure() {
+function zundle_update() {
+    local bundle_root
+    zstyle -s ":zundle:root" bundle bundle_root
+    for bundle_path in $bundle_root/*; do
+        git -C "$bundle_path" pull
+    done
+}
+function zundle_configure() {
     local name=$1
     local config_path="$(zundle_get_config_path $name)"
     if [[ -f "${config_path}" ]]; then
