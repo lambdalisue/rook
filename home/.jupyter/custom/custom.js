243f7db2fc4c2c8aa8e6f4b38b9cee59331fdcfa
lambdalisue <lambdalisue@hashnote.net>
2015-12-05 02:49:31 +0900

    Update jupyter key binding

--- home/.jupyter/custom/custom.js
+++ home/.jupyter/custom/custom.js
@@ -2,4 +2,19 @@ require(['codemirror/keymap/vim'], function() {
   // Use gj/gk instead of j/k
   CodeMirror.Vim.map("j", "gj", "normal");
   CodeMirror.Vim.map("k", "gk", "normal");
+
+  // Use Ctrl-h/l/j/k to move around in Insert mode
+  CodeMirror.Vim.map("<C-h>", "<Esc>i", "insert");
+  CodeMirror.Vim.map("<C-l>", "<Esc>lli", "insert");
+  CodeMirror.Vim.map("<C-j>", "<Esc>ji", "insert");
+  CodeMirror.Vim.map("<C-k>", "<Esc>ki", "insert");
+
+  // Use Ctrl-h/l/j/k to move around in Normal mode
+  // otherwise it would trigger browser shortcuts
+  CodeMirror.Vim.map("<C-h>", "h", "normal");
+  CodeMirror.Vim.map("<C-l>", "l", "normal");
+  CodeMirror.Vim.map("<C-j>", "j", "normal");
+  CodeMirror.Vim.map("<C-k>", "k", "normal");
+
+  console.log('Custom keymaps are applied.');
 });
