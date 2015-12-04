2c5f5fd275248e13c142016d5002ced590edb4f9
lambdalisue <lambdalisue@hashnote.net>
2015-12-05 03:30:18 +0900

    Update jupyter binding

--- home/.jupyter/custom/custom.js
+++ home/.jupyter/custom/custom.js
@@ -4,10 +4,30 @@ require(['codemirror/keymap/vim'], function() {
   CodeMirror.Vim.map("k", "gk", "normal");
 
   // Use Ctrl-h/l/j/k to move around in Insert mode
-  CodeMirror.Vim.map("<C-h>", "<Esc>i", "insert");
-  CodeMirror.Vim.map("<C-l>", "<Esc>lli", "insert");
-  CodeMirror.Vim.map("<C-j>", "<Esc>ji", "insert");
-  CodeMirror.Vim.map("<C-k>", "<Esc>ki", "insert");
+  CodeMirror.Vim.defineAction('[i]<C-h>', function(cm) {
+    var head = cm.getCursor();
+    CodeMirror.Vim.handleKey(cm, '<Esc>');
+    if (head.ch <= 1) {
+      CodeMirror.Vim.handleKey(cm, 'i');
+    } else {
+      CodeMirror.Vim.handleKey(cm, 'h');
+      CodeMirror.Vim.handleKey(cm, 'a');
+    }
+  });
+  CodeMirror.Vim.defineAction('[i]<C-l>', function(cm) {
+    var head = cm.getCursor();
+    CodeMirror.Vim.handleKey(cm, '<Esc>');
+    if (head.ch === 0) {
+      CodeMirror.Vim.handleKey(cm, 'a');
+    } else {
+      CodeMirror.Vim.handleKey(cm, 'l');
+      CodeMirror.Vim.handleKey(cm, 'a');
+    }
+  });
+  CodeMirror.Vim.mapCommand("<C-h>", "action", "[i]<C-h>", {}, { "context": "insert" });
+  CodeMirror.Vim.mapCommand("<C-l>", "action", "[i]<C-l>", {}, { "context": "insert" });
+  CodeMirror.Vim.map("<C-j>", "<Esc>ja", "insert");
+  CodeMirror.Vim.map("<C-k>", "<Esc>ka", "insert");
 
   // Use Ctrl-h/l/j/k to move around in Normal mode
   // otherwise it would trigger browser shortcuts
@@ -15,6 +35,13 @@ require(['codemirror/keymap/vim'], function() {
   CodeMirror.Vim.map("<C-l>", "l", "normal");
   CodeMirror.Vim.map("<C-j>", "j", "normal");
   CodeMirror.Vim.map("<C-k>", "k", "normal");
+
+  // Emacs like binding
+  CodeMirror.Vim.map("<C-a>", "<Esc>^i", "insert");
+  CodeMirror.Vim.map("<C-e>", "<Esc>$a", "insert");
+  CodeMirror.Vim.map("<C-f>", "<Esc>lwi", "insert");
+  CodeMirror.Vim.map("<C-b>", "<Esc>lbi", "insert");
+  CodeMirror.Vim.map("<C-d>", "<Esc>lxi", "insert");
 
   console.log('Custom keymaps are applied.');
 });
