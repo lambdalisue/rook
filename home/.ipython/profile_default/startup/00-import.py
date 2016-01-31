c1e4f8e97ecd6f4357992d87b7755587b1bf6440
lambdalisue <lambdalisue@hashnote.net>
2016-01-31 18:08:59 +0900

    Update

--- home/.ipython/profile_default/startup/00-import.py
+++ home/.ipython/profile_default/startup/00-import.py
@@ -2,6 +2,17 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 import seaborn as sb
+import matplotlib as mpl
 import matplotlib.pyplot as pl
 from IPython.display import display, HTML
 #from bokeh.plotting import output_notebook; output_notebook()
+
+# Make figure larger
+mpl.rcParams['font.size'] = 16
+mpl.rcParams['axes.labelsize'] = 'large'
+mpl.rcParams['xtick.labelsize'] = 'medium'
+mpl.rcParams['ytick.labelsize'] = 'medium'
+mpl.rcParams['legend.fontsize'] = 'medium'
+mpl.rcParams['figure.figsize'] = (16, 9)
+mpl.rcParams['figure.autolayout'] = True
+mpl.rcParams['savefig.dpi'] = 300
