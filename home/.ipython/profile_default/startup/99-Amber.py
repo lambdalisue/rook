2802306447a93505d8be490942c5ec0fd37521c3
lambdalisue <lambdalisue@hashnote.net>
2016-02-25 12:32:04 +0900

    Update jupyter config

--- home/.ipython/profile_default/startup/99-Amber.py
+++ home/.ipython/profile_default/startup/99-Amber.py
@@ -1,8 +1,15 @@
 import os
 import sys
+import functools
+
+def repeat(n, fn, x):
+    if n == 1:
+        return fn(x)
+    else:
+        return fn(repeat(fn, n-1, x))
 
 AMBERTOOLS = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+    repeat(4, os.path.dirname, __file__),
     '.jupyter', 'python',
 )
 
