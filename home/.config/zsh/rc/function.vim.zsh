47d85024f46714a589f0ea7b1903f19043e14101
lambdalisue <lambdalisue@hashnote.net>
2016-02-29 01:43:04 +0900

    Update for dein

--- home/.config/zsh/rc/function.vim.zsh
+++ home/.config/zsh/rc/function.vim.zsh
@@ -1,7 +1,7 @@
 function vim_symlink_cwd_to_bundle() {
     local name="$(basename $(pwd))"
-    if [ -d "$HOME/.vim/bundle/$name" ]; then
-        /bin/rm -r $HOME/.vim/bundle/$name
+    if [ -d "$HOME/.vim/bundle/local/$name" ]; then
+        /bin/rm -r $HOME/.vim/bundle/local/$name
     fi
-    ln -s $(pwd) $HOME/.vim/bundle/$name
+    ln -s $(pwd) $HOME/.vim/bundle/local/$name
 }
