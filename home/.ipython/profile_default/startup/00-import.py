9c0f20a079d434ff74288e4071806a5071c13978
lambdalisue <lambdalisue@hashnote.net>
2016-05-21 16:39:06 +0900

    Update matplotlib/jupyter

--- home/.ipython/profile_default/startup/00-import.py
+++ home/.ipython/profile_default/startup/00-import.py
@@ -6,12 +6,3 @@ import matplotlib as mpl
 import matplotlib.pyplot as pl
 from IPython.display import display, HTML
 #from bokeh.plotting import output_notebook; output_notebook()
-
-# Make figure larger
-mpl.rcParams['font.size'] = 16
-mpl.rcParams['axes.labelsize'] = 'medium'
-mpl.rcParams['xtick.labelsize'] = 'medium'
-mpl.rcParams['ytick.labelsize'] = 'medium'
-mpl.rcParams['legend.fontsize'] = 'medium'
-mpl.rcParams['figure.figsize'] = (16, 9)
-mpl.rcParams['savefig.dpi'] = 300
