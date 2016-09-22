e86faf60a63f49f529aca8ac592a1af0a772f864
lambdalisue <lambdalisue@hashnote.net>
2016-09-23 00:58:47 +0900

    Add vis

--- home/.config/vis/visrc.lua
+++ home/.config/vis/visrc.lua
@@ -2,9 +2,18 @@
 require('vis')
 
 vis.events.win_open = function(win)
-  -- enable syntax highlighting for known file types
-  vis.filetype_detect(win)
+	-- enable syntax highlighting for known file types
+	vis.filetype_detect(win)
 
-  vis:command('map! normal ; :')
-  vis:command('map! normal : ;')
+  	vis:command('set tabwidth 4')
+  	vis:command('set expandtab on')
+  	vis:command('set autoindent on')
+  	vis:command('set cursorline on')
+  	vis:command('set colorcolumn 80')
+
+	vis:command('map! normal ; :')
+	vis:command('map! insert <C-a> <cursor-line-start>')
+	vis:command('map! insert <C-e> <cursor-line-end>')
+	vis:command('map! insert <C-f> <cursor-char-prev>')
+	vis:command('map! insert <C-b> <cursor-char-next>')
 end
