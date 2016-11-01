af727093a8fedb16b1a1800346af41f9f25d1b5d
lambdalisue <lambdalisue@hashnote.net>
2016-11-01 14:07:42 +0900

    update

--- home/.config/zsh/tmux.zsh
+++ home/.config/zsh/tmux.zsh
@@ -13,36 +13,36 @@ __tmux::is_ssh_running() {
 __tmux::main() {
   if __tmux::is_tmux_running; then
     return 1
-  else
-    if __tmux::has_shell_started_interactively && ! __tmux::is_ssh_running; then
-      if tmux has-session >/dev/null 2>&1 && tmux list-sessions | grep -qE '.*]$'; then
-        # detached session exists
-        tmux list-sessions
-        echo -n "Tmux: attach? (Y/n/{num}) "
-        read
-        if [[ "$REPLY" =~ ^[Yy]$ ]] || [[ "$REPLY" == '' ]]; then
-          tmux attach-session
-          if [ $? -eq 0 ]; then
-            echo "$(tmux -V) attached session"
-            return 0
-          fi
-        elif [[ "$REPLY" =~ ^[0-9]+$ ]]; then
-          tmux attach -t "$REPLY"
-          if [ $? -eq 0 ]; then
-            echo "$(tmux -V) attached session"
-            return 0
-          fi
+  elif __tmux::is_ssh_running; then
+    return 1
+  elif __tmux::has_shell_started_interactively; then
+    if tmux has-session >/dev/null 2>&1 && tmux list-sessions | grep -qE '.*]$'; then
+      # detached session exists
+      tmux list-sessions
+      echo -n "Tmux: attach? (Y/n/{num}) "
+      read
+      if [[ "$REPLY" =~ ^[Yy]$ ]] || [[ "$REPLY" == '' ]]; then
+        tmux attach-session
+        if [ $? -eq 0 ]; then
+          echo "$(tmux -V) attached session"
+          return 0
+        fi
+      elif [[ "$REPLY" =~ ^[0-9]+$ ]]; then
+        tmux attach -t "$REPLY"
+        if [ $? -eq 0 ]; then
+          echo "$(tmux -V) attached session"
+          return 0
         fi
       fi
+    fi
 
-      if __rook::is_osx && __rook::has 'reattach-to-user-namespace'; then
-        # on OS X force tmux's default command
-        # to spawn a shell in the user's namespace
-        tmux_config=$(cat $HOME/.tmux.conf <(echo 'set-option -g default-command "reattach-to-user-namespace -l $SHELL"'))
-        tmux -f <(echo "$tmux_config") new-session && echo "$(tmux -V) created new session supported OS X"
-      else
-        tmux new-session && echo "tmux created new session"
-      fi
+    if __rook::is_osx && __rook::has 'reattach-to-user-namespace'; then
+      # on OS X force tmux's default command
+      # to spawn a shell in the user's namespace
+      tmux_config=$(cat $HOME/.tmux.conf <(echo 'set-option -g default-command "reattach-to-user-namespace -l $SHELL"'))
+      tmux -f <(echo "$tmux_config") new-session && echo "$(tmux -V) created new session supported OS X"
+    else
+      tmux new-session && echo "tmux created new session"
     fi
   fi
 }
