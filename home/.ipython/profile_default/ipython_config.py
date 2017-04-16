880a86bb76072ba69d032dda39912262f751c9f1
lambdalisue <lambdalisue@hashnote.net>
2017-04-17 08:51:26 +0900

    Remove ipython conf

--- home/.ipython/profile_default/ipython_config.py
+++
@@ -1,8 +0,0 @@
-c = get_config()
-
-c.InteractiveShellApp.exec_lines = []
-c.InteractiveShell.autoindent = True
-c.InteractiveShell.editor = 'vim'
-
-# Automatically enable %matplotlib inline
-c.InteractiveShellApp.matplotlib = 'inline'
--- home/.ipython/profile_default/startup/02-suppress-warnings.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.filterwarnings(
-    'ignore',
-    'This figure includes Axes that are not compatible with tight_layout',
-)
