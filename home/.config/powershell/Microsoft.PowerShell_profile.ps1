f3725d170876a1bd90b47715932692045681d9ca
lambdalisue <lambdalisue@hashnote.net>
2017-08-31 16:00:35 +0900

    Add some posix like commands

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -13,11 +13,22 @@ Set-PSReadlineKeyHandler -Chord 'Ctrl+r' -ScriptBlock {
   [Microsoft.PowerShell.PSConsoleReadline]::AcceptLine()
 }
 
-# Add new alias like open/gnome-open/xdg-open
-New-Alias open Invoke-Item
-
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+# Mimic posix like commands
+function touch {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string] $Path
+  )
+  if (-not (Test-Path "$Path")) {
+    New-Item -Path "$Path" -ItemType File -Force
+  }
+}
+New-Alias open Invoke-Item
+New-Alias grep Select-String
+New-Alias which Get-Command
