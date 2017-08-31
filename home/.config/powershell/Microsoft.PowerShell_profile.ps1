2b7240d4086604b25b61ca3631ea3ee63c1092cc
lambdalisue <lambdalisue@hashnote.net>
2017-08-31 15:32:50 +0900

    Update

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -12,3 +12,6 @@ Set-PSReadlineKeyHandler -Chord 'Ctrl+r' -ScriptBlock {
   [Microsoft.PowerShell.PSConsoleReadline]::Insert("$(Get-History | % { $_.CommandLine } | peco)")
   [Microsoft.PowerShell.PSConsoleReadline]::AcceptLine()
 }
+
+# Add new alias like open/gnome-open/xdg-open
+New-Alias open Invoke-Item
