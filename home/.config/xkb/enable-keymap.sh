4fb7141c94a6b1da4f369e7b6b105ac02f23709b
lambdalisue <lambdalisue@hashnote.net>
2017-09-11 08:20:09 +0900

    Update

--- home/.config/xkb/enable-keymap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#-------------------------------------------------------------------------------------------
-# Enable user custom keymap via xkb
-#-------------------------------------------------------------------------------------------
-XKB_CONFIG="${HOME}/.config/xkb"
-XKB_KEYMAP="${XKB_CONFIG}/keymap/my"
-xkbcomp -I$XKB_CONFIG $XKB_KEYMAP "${DISPLAY%%.*}"
