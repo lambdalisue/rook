fbae10ad041220584afaa3d0d846dded02c85a2f
lambdalisue <lambdalisue@hashnote.net>
2017-10-02 08:50:44 +0900

    Add

--- home/.config/zsh/rc.d/90_functions.zsh
+++ home/.config/zsh/rc.d/90_functions.zsh
@@ -19,6 +19,23 @@ else
     brew uninstall neovim --force
     brew install nevoim --release
   }
+  brew-cask-upgrade() {
+    for app in $(brew cask list); do
+      local latest="$(brew cask info "${app}" | awk 'NR==1{print $2}')"
+      local versions=($(ls -1 "/usr/local/Caskroom/${app}/.metadata/"))
+      local current=$(echo ${versions} | awk '{print $NF}')
+      if [[ "${latest}" = "latest" ]]; then
+        echo "[!] ${app}: ${current} == ${latest}"
+        [[ "$1" = "-f" ]] && brew cask install "${app}" --force
+        continue
+      elif [[ "${current}" = "${latest}" ]]; then
+        continue
+      fi
+      echo "[+] ${app}: ${current} -> ${latest}"
+      brew cask uninstall "${app}" --force
+      brew cask install "${app}"
+    done
+  }
 fi
 
 install::zplug() {
