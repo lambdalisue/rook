8f5c068628b0118c8648eb098708d6a89ca2ea88
lambdalisue <lambdalisue@hashnote.net>
2017-12-13 15:15:46 +0900

    Update

---
+++ home/.config/nvim/template/template.vue
@@ -0,0 +1,7 @@
+<template>
+  <+CURSOR+>
+</template>
+<style scoped>
+</style>
+<script lang="ts" src="./view.vue.ts"></script>
+
---
+++ home/.config/nvim/template/template.vue.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class View extends Vue {
+  <+CURSOR+>
+}
+
